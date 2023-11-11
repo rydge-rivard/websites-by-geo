cf58ce7d12e9b6438ae2699d90d9bf66ed4c3bf2
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-11-11 22:41:24 +0100

    Connect API and load map.

---
+++ index.js
@@ -0,0 +1,21 @@
+const mapMOD = (function () {
+  //   console.log("test");
+})();
+
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+}
