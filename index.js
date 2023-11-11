a11ef05b832470c306030e24058f5d9ca3c05d40
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-11-12 00:24:20 +0100

    Query data and return array in console with selected fields.

--- index.js
+++ index.js
@@ -1,21 +1,72 @@
-const mapMOD = (function () {
-  //   console.log("test");
-})();
+let map = "";
+let service = "";
+let infowindow = "";
+const supermarkets = [];
+const prospectList = [];
 
-// Initialize and add the map
-let map;
+function initialize() {
+  const simcoe = new google.maps.LatLng(42.8373, -80.304042);
 
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: simcoe,
+    zoom: 15,
   });
+
+  let request = {
+    // location: simcoe,
+    // radius: "5",
+    query: "butcher shop simcoe on canada",
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, createSupermarketList);
+}
+
+function createSupermarketList(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+      let place = results[i];
+      supermarkets.push(place);
+    }
+  }
+  getQueryData();
+  console.log(prospectList);
+}
+
+function getQueryData() {
+  getProspectIds(supermarkets).forEach((prospect) => {
+    getProspectData(prospect);
+  });
+}
+
+function getProspectIds(areaSearch) {
+  prospectIds = [];
+
+  supermarkets.forEach((prospect) => {
+    prospectIds.push(prospect.place_id);
+  });
+  return prospectIds;
+}
+
+function getProspectData(placeId) {
+  let request = {
+    placeId: placeId,
+    fields: [
+      "name",
+      "formatted_address",
+      "website",
+      "business_status",
+      "international_phone_number",
+      "type",
+    ],
+  };
+  prospect = new google.maps.places.PlacesService(map);
+  prospect.getDetails(request, createProspectList);
+}
+
+function createProspectList(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    prospectList.push(results);
+    console.log(prospectList);
+  }
 }
